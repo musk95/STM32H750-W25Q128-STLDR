56981db894f41db8f400bb6dfbd68e8c9099ef73
alex.hong <alex.hong@st.com>
2021-10-22 11:48:27 +0900

    * support Verify while programming while downloading

--- Core/Src/Loader_Src.c
+++ Core/Src/Loader_Src.c
@@ -288,33 +288,118 @@ int Read (uint32_t Address, uint32_t Size, uint8_t* Buffer)
     return 1;
 }
 
-/*******************************************************************************
- Description :
- Verify the data
- Inputs :
- 				MemoryAddr 	: Write location
- 				RAMBufferAddr 	: RAM Address
- 				Size 		: Length in bytes
- outputs :
- 				"0" 		: Operation succeeded
- Note : Not Mandatory
-********************************************************************************/
-int Verify (uint32_t MemoryAddr, uint32_t RAMBufferAddr, uint32_t Size)
+/**
+  * Description :
+  * Calculates checksum value of the memory zone
+  * Inputs    :
+  *      StartAddress  : Flash start address
+  *      Size          : Size (in WORD)
+  *      InitVal       : Initial CRC value
+  * outputs   :
+  *     R0             : Checksum value
+  * Note: Optional for all types of device
+  */
+uint32_t CheckSum(uint32_t StartAddress, uint32_t Size, uint32_t InitVal)
 {
-    uint32_t VerifiedData = 0;
-    Size*=4;
+  uint8_t missalignementAddress = StartAddress%4;
+  uint8_t missalignementSize = Size;
+  int cnt;
+  uint32_t Val;
 
-    W25Q128F_MemoryMapped();
+  StartAddress-=StartAddress%4;
+  Size += (Size%4==0)?0:4-(Size%4);
 
-    while (Size>VerifiedData)
+  for(cnt=0; cnt<Size ; cnt+=4)
+  {
+    Val = *(uint32_t*)StartAddress;
+    if(missalignementAddress)
     {
-        if ( *(uint8_t*)MemoryAddr++ != *((uint8_t*)RAMBufferAddr + VerifiedData))
-            return (MemoryAddr + VerifiedData);
-
-        VerifiedData++;
+      switch (missalignementAddress)
+      {
+        case 1:
+          InitVal += (uint8_t) (Val>>8 & 0xff);
+          InitVal += (uint8_t) (Val>>16 & 0xff);
+          InitVal += (uint8_t) (Val>>24 & 0xff);
+          missalignementAddress-=1;
+          break;
+        case 2:
+          InitVal += (uint8_t) (Val>>16 & 0xff);
+          InitVal += (uint8_t) (Val>>24 & 0xff);
+          missalignementAddress-=2;
+          break;
+        case 3:
+          InitVal += (uint8_t) (Val>>24 & 0xff);
+          missalignementAddress-=3;
+          break;
+      }
     }
+    else if((Size-missalignementSize)%4 && (Size-cnt) <=4)
+    {
+      switch (Size-missalignementSize)
+      {
+        case 1:
+          InitVal += (uint8_t) Val;
+          InitVal += (uint8_t) (Val>>8 & 0xff);
+          InitVal += (uint8_t) (Val>>16 & 0xff);
+          missalignementSize-=1;
+          break;
+        case 2:
+          InitVal += (uint8_t) Val;
+          InitVal += (uint8_t) (Val>>8 & 0xff);
+          missalignementSize-=2;
+          break;
+        case 3:
+          InitVal += (uint8_t) Val;
+          missalignementSize-=3;
+          break;
+      }
+    }
+    else
+    {
+      InitVal += (uint8_t) Val;
+      InitVal += (uint8_t) (Val>>8 & 0xff);
+      InitVal += (uint8_t) (Val>>16 & 0xff);
+      InitVal += (uint8_t) (Val>>24 & 0xff);
+    }
+    StartAddress+=4;
+  }
 
-    return 0;
+  return (InitVal);
+}
+
+
+/**
+  * Description :
+  * Verify flash memory with RAM buffer and calculates checksum value of
+  * the programmed memory
+  * Inputs    :
+  *      FlashAddr     : Flash address
+  *      RAMBufferAddr : RAM buffer address
+  *      Size          : Size (in WORD)
+  *      InitVal       : Initial CRC value
+  * outputs   :
+  *     R0             : Operation failed (address of failure)
+  *     R1             : Checksum value
+  * Note: Optional for all types of device
+  */
+uint64_t Verify (uint32_t MemoryAddr, uint32_t RAMBufferAddr, uint32_t Size, uint32_t missalignement)
+{
+	uint32_t VerifiedData = 0, InitVal = 0;
+	uint64_t checksum;
+	Size*=4;
+
+	W25Q128F_MemoryMapped();
+
+	checksum = CheckSum((uint32_t)MemoryAddr + (missalignement & 0xF), Size - ((missalignement >> 16) & 0xF), InitVal);
+	while (Size>VerifiedData)
+	{
+		if ( *(uint8_t*)MemoryAddr++ != *((uint8_t*)RAMBufferAddr + VerifiedData))
+			return ((checksum<<32) + (MemoryAddr + VerifiedData));
+
+		VerifiedData++;
+	}
+
+	return (checksum<<32);
 }
 
 void *memset(void *buf, int ch, size_t n)
